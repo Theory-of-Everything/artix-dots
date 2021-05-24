3aac93f9d9231a785907fd7d29469b2724739642
Theory_of_Everything <evan_sarris@protonmail.ch>
2021-05-23 18:09:05 -0600

    updated incorrect package in script

--- packages.sh
+++ packages.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 pacman -Syyu -noconfirm
-pacman -S awesome lightdm lightdm-gtk3-greeter nvim neofetch htop picom rofi urxvt
+pacman -S awesome lightdm-openrc lightdm-gtk3-greeter nvim neofetch htop picom rofi urxvt
